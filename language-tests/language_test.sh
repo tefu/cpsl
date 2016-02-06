5215773d3c2daf5a825b332aaa0a80f643162539
Trevor Hullinger <tshullinger@gmail.com>
2016-02-06 04:42:59 -0700

    Change filename printed when running language tests

--- language-tests/language_test.sh
+++ language-tests/language_test.sh
@@ -1,7 +1,7 @@
 for program in $(ls test-files)
 do
     echo "-------------------------------------------------"
-    echo "Running compiler on ${program}... "
+    echo "Running compiler on ./test-files/${program}... "
     echo "-------------------------------------------------"
 	  cat "test-files/$program" | ./cpsl
 done
