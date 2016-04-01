49b9a2fade84c2ce8e4d5ed3001a4bce65b70026
Trevor Hullinger <tshullinger@gmail.com>
2016-04-01 09:14:42 -0600

    Remove indirect all_correct function

--- src/Function.cpp
+++ src/Function.cpp
@@ -1,23 +1,5 @@
 #include "Function.hpp"
 
-namespace
-{
-  template<typename F>
-  bool all_correct(F correct, Function func, std::vector<Expression*> exprList)
-  {
-    for(auto i = 0; i < func.parameters.size() && i < exprList.size(); i++)
-      {
-        auto param = func.parameters[i];
-        auto expr = exprList[i];
-        if(!correct(param,expr))
-        {
-          return false;
-        }
-      }
-    return true;
-  }
-}
-
 bool Function::same_signature(const Function& other_func)
 {
   if (parameters.size() != other_func.parameters.size())
@@ -54,7 +36,20 @@ bool Function::correct_types(std::vector<Type*> types)
 
 bool Function::correct_references(std::vector<Expression*> exprList)
 {
-  return all_correct([](FormalParameter* param, Expression* expr){
-      return (param->is_variable || expr->can_be_referenced());
-    }, *this, exprList);
+  if(parameters.size() != exprList.size())
+  {
+    return false;
+  }
+
+  for(auto i = 0; i < parameters.size() && i < exprList.size(); i++)
+  {
+    auto param = parameters[i];
+    auto expr = exprList[i];
+    if(!(param->is_variable || expr->can_be_referenced()))
+    {
+      return false;
+    }
+  }
+
+  return true;
 }
